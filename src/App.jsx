d9cbd949a7379fc1f2288f83433bdeb2e3d39f94
silvacook <robertsilva1994@yahoo.com>
2023-10-11 19:36:09 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { 
+  About, 
+  HomeLayout, 
+  Landing, 
+  Error, 
+  Newsletter, 
+  Cocktail,
+  SinglePageError,
+} from './pages';
+
+import { loader as landingLoader } from "./pages/Landing";
+import { loader as singleCocktailLoader } from "./pages/Cocktail";
+import { action as newsletterAction } from "./pages/Newsletter";
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: '/',
+        element: <Landing />,
+        errorElement: <SinglePageError />,
+        loader: landingLoader,
+      },
+      {
+        path: 'cocktail/:id',
+        errorElement: <SinglePageError />,
+        loader: singleCocktailLoader,
+        element: <Cocktail />,
+      },
+      {
+        path: 'newsletter',
+        element: <Newsletter />,
+        action: newsletterAction,
+      },
+      {
+        path: 'about',
+        element: <About />,
+        children: [
+          {
+            path: 'company',
+            element: <h2>our company</h2>,
+          },
+          {
+            path: 'person',
+            element: <h2>john doe</h2>,
+          },
+        ]
+      },
+    ]
+  },
+ 
+])
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+export default App;
---
+++ src/pages/HomeLayout.jsx
@@ -0,0 +1,19 @@
+import {Outlet, useNavigation} from "react-router-dom"
+import Navbar from "../components/Navbar";
+
+const Homelayout = () => {
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === "loading";
+    return (
+      <>
+        <Navbar />
+        <section className="page">
+          {isPageLoading?( <div className="loading"/> ): (
+             <Outlet/> 
+             )}
+        </section>
+      </> 
+    )
+  }
+  
+  export default Homelayout;
